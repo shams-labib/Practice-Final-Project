2057ce844f030358c03f09582bf8bdb16685d013
shams al labib <samslabib19@gmail.com>
2025-11-19 13:25:01 +0600

    added slider

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -4,13 +4,15 @@ import Brands from "../Brands/Brands";
 import Reviews from "../Reviews/Reviews";
 import Container from "../../Shared/Container/Container";
 
+const reviewPromise = fetch("./reviews.json").then((res) => res.json());
+
 const Home = () => {
   return (
     <Container>
       <div>
         <Bannar></Bannar>
         <Brands></Brands>
-        <Reviews></Reviews>
+        <Reviews reviewPromise={reviewPromise}></Reviews>
       </div>
     </Container>
   );
--- src/Pages/Reviews/Reviews.jsx
+++ src/Pages/Reviews/Reviews.jsx
@@ -1,7 +1,55 @@
-import React from "react";
+import React, { use } from "react";
+import { Autoplay, EffectCoverflow, Pagination } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import ReviewsCard from "./ReviewsCard";
 
-const Reviews = () => {
-  return <div></div>;
+const Reviews = ({ reviewPromise }) => {
+  const reviewsData = use(reviewPromise);
+  console.log(reviewsData);
+
+  return (
+    <div className="my-24">
+      <div className="text-center mb-24">
+        <h3 className="text-center mt-10 text-3xl font-semibold">Review </h3>
+        <p className="font-bold">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Animi
+          voluptatem unde sapiente aspernatur. Facilis, magni at ad nesciunt ex
+          illo architecto enim aperiam, voluptate itaque adipisci corporis
+          possimus, aliquid eos?
+        </p>
+      </div>
+      <>
+        <Swiper
+          loop={true}
+          effect={"coverflow"}
+          grabCursor={true}
+          centeredSlides={true}
+          slidesPerView={3}
+          coverflowEffect={{
+            rotate: 30,
+            stretch: "50%",
+            depth: 200,
+            scale: 0.75,
+            modifier: 1,
+            slideShadows: true,
+          }}
+          autoplay={{
+            delay: 2000,
+            disableOnInteraction: false,
+          }}
+          pagination={true}
+          modules={[EffectCoverflow, Pagination, Autoplay]}
+          className="mySwiper"
+        >
+          {reviewsData.map((review) => (
+            <SwiperSlide className="mt-10" key={review.id}>
+              <ReviewsCard review={review}></ReviewsCard>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </>
+    </div>
+  );
 };
 
 export default Reviews;
